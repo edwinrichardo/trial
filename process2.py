37dce08f8fb5ca5175842d5bf4007222125b15b8
erichardo <Edwin.Richardo@agoda.com>
2019-04-07 14:48:08 +0800

    test

--- process2.py
+++ process2.py
@@ -1,5 +1,7 @@
 #This is a new process that I would like to propse
 #It is just a skeleton, nothing created yet
 
+
 import pandas as pd
-pd = read_csv("C:/Docs/edwin/raja.xlsx")!
+xy = read_csv("C:/Docs/edwin/raja.xlsx")!
+xy.groupby("TEST")["hari","manu","jojon"].sum()
