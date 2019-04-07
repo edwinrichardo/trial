3ffc91fb08c7af50ddf917204bdf84e75626ee20
erichardo <Edwin.Richardo@agoda.com>
2019-04-07 14:43:13 +0800

    other use will update

--- process2.py
+++ process2.py
@@ -1,2 +1,5 @@
 #This is a new process that I would like to propse
-#It is just a skeleton, nothing created yet!
+#It is just a skeleton, nothing created yet
+
+import pandas as pd
+pd = read_csv("C:/Docs/edwin/raja.xlsx")!
